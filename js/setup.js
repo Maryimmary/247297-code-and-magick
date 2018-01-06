5ce06670314c8dfdd8e549ca540800e7c3bf9306
Maryimmary <maryimmary@yandex.ru>
2018-01-06 17:04:26 +0300

    fixed for eslint

--- js/setup.js
+++ js/setup.js
@@ -4,14 +4,14 @@ var setup = document.querySelector('.setup');
 setup.classList.remove('hidden');
 var setupSimilar = setup.querySelector('.setup-similar');
 setupSimilar.classList.remove('hidden');
-var data = [ ];
+var data = [];
 var firstNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var colorCoat = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var colorEyes = ['black', 'red', 'blue', 'yellow', 'green'];
 
 function getRandomItem(arr) {
- return arr[Math.floor(Math.random() * arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
 function pushArray(arr) {
@@ -21,7 +21,7 @@ function pushArray(arr) {
       coatColor: getRandomItem(colorCoat),
       eyesColor: getRandomItem(colorEyes)
     };
-    arr.push(dataObject)
+    arr.push(dataObject);
   }
 }
 pushArray(data);
@@ -36,7 +36,7 @@ function createElement() {
 }
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i<data.length; i++) {
+for (var i = 0; i < data.length; i++) {
   fragment.appendChild(createElement());
 }
 similarList.appendChild(fragment);
